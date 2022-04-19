ac3b07c51bf07552674b6eddcd8214b6006230f9
seaboyz <seaboyz@hotmail.com>
2022-04-19 12:46:22 -0500

    Change route to /users

---
+++ src/main/java/UserServlet.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<h2>Getting all users from database</h2>");
+  }
+}
